236c58186782a07202e54377c1217b4ad941004d
Tao Lin <574278812@qq.com>
2021-10-12 12:26:54 +0800

    Committed by Lin 2021.10.12 11:35

--- src/views/employee/Ho_Record.vue
+++ src/views/employee/Ho_Record.vue
@@ -1,16 +1,19 @@
 <template>
   <el-tabs :tab-position="tabPosition" type="border-card" v-model="activeName">
-    <el-tab-pane label="考勤" name="AttendanceRecord"><AttendanceRecord></AttendanceRecord></el-tab-pane>
+    <el-tab-pane label="考勤记录" name="AttendanceRecord"><AttendanceRecord></AttendanceRecord></el-tab-pane>
 
 
   </el-tabs>
 </template>
 
 <script>
+
 import AttendanceRecord from "../../components/sys/AttendanceRecord";
+
 
 export default {
   name: "Ho_Record",
+  components: {AttendanceRecord},
   data() {
     return {
       tabPosition: 'bottom',//'top',
